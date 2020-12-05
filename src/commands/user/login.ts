8ca664d380e6a756a34c4b8ad94060f38cca63fc
Indexyz <jiduye@gmail.com>
2020-12-05 12:55:04 +0800

    feat: allow re-login

--- src/commands/user/login.ts
+++ src/commands/user/login.ts
@@ -10,45 +10,56 @@ export default class LoginCommand extends Command {
     static examples = [
         '$ cloudreve-cli user:login https://example.com\n' +
         'Login to cloudreve running at https://example.com',
+
+        '$ cloudreve-cli user:login -r\n' +
+        'Re-login to localhost',
     ]
 
     static flags = {
         headless: flags.boolean({ char: 'h', description: 'Running login with headless mode' }),
         email: flags.string({ char: 'u', description: 'Login email' }),
         password: flags.string({ char: 'p', description: 'Login password' }),
+        relogin: flags.boolean({ char: 'r', description: 'Re-login using local certification' }),
     }
 
     static args = [{
         name: 'siteUrl',
-        required: true,
+        required: false,
     }]
 
     async run(): Promise<null> {
         // eslint-disable-next-line @typescript-eslint/no-shadow
         const { args, flags } = this.parse(LoginCommand)
 
-        await config.set('site:url', args.siteUrl)
-        this.log('Warning: Password will store with clear-text!')
+        if (!args.siteUrl && !flags.relogin) {
+            this.log('Error: please set siteUrl')
+            process.exit(1)
+        }
 
-        if (flags.headless) {
-            if (flags.email) {
-                await config.set('site:email', flags.email)
-            } else {
-                this.log('Please use -u <email> to enter email')
-                process.exit(1)
-            }
+        if (!flags.relogin) {
+            await config.set('site:url', args.siteUrl)
+            this.log('Warning: Password will store with clear-text!')
 
-            if (flags.password) {
-                await config.set('site:password', flags.password)
+            if (flags.headless) {
+                if (flags.email) {
+                    await config.set('site:email', flags.email)
+                } else {
+                    this.log('Please use -u <email> to enter email')
+                    process.exit(1)
+                }
+
+                if (flags.password) {
+                    await config.set('site:password', flags.password)
+                } else {
+                    this.log('Please use -p <password> to enter password')
+                    process.exit(1)
+                }
             } else {
-                this.log('Please use -p <password> to enter password')
-                process.exit(1)
+                await config.set('site:email', await ask('Input email: '))
+                await config.set('site:password', await ask('Input password: ', {
+                    hidden: true,
+                }))
             }
-        } else {
-            await config.set('site:email', await ask('Input email: '))
-            await config.set('site:password', await ask('Input password: ', {
-                hidden: true,
-            }))
         }
 
         const spinner = ora('登录中').start();
