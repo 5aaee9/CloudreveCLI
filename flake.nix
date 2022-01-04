b6c85c121e580555c16926fb81fcf47d4bd6c1ba
Indexyz <jiduye@gmail.com>
2022-01-04 18:50:01 +0800

    fix: output with darwin support

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ]
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ]
       (system:
         let
           pkgs = import nixpkgs {
